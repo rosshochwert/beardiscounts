62c5a8485cc070e1d1060f1d44fe9b0043417976
Ross Hochwert <rosshochwert@gmail.com>
2013-06-25 19:56:12 -0500

    Configure for deployment

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,5 @@
 class Discount < ActiveRecord::Base
-  attr_accessible :description, :location, :logo, :name, :picture
+  attr_accessible :description, :location, :logo, :name, :picture, :picture_file_name
   has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "/images/:style/missing.png"
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "/images/:style/missing.png"
 
