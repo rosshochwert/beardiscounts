3f5907fe65200cab38a71603095802809e6e161b
Ross Hochwert <rosshochwert@gmail.com>
2013-11-02 13:57:37 -0500

    Silly syntax error

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -27,7 +27,7 @@ class CustomersController < ApplicationController
     @customer = Customer.find_by_hashcode(params[:hashcode])
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @customer], :callback => params[:callback]}
+      format.json { render json: @customer, :callback => params[:callback]}
     end
   end
 
